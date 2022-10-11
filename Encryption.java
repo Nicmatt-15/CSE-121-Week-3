c70f87e800da9c8d1c229e8c7469f5ad4e24a0f8
Nicolas <nicmatt@uw.edu>
2022-10-11 09:00:20 -0700

    added starter code

---
+++ Encryption.java
@@ -0,0 +1,13 @@
+public class Encryption {
+    public static void main(String[] args) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int shift = 3;
+
+        char toBeEncrypted = 'a';
+        char encryptedLetter;
+
+        // Your code below
+
+        System.out.println("Encrypted letter is: " + encryptedLetter);
+    }
+}
